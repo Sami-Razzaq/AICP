fc75bd6e00a3dff1e3c6cb5f191c0c2ae7754587
Sami <samirazzaq@outlook.com>
2024-07-09 17:03:54 +0500

    Task 1 Q5

--- Task 1/task1.py
+++ Task 1/task1.py
@@ -13,18 +13,28 @@ data = pd.read_csv('Instagram-Reach.csv')
 # print(data.head())
 data['Date'] = pd.to_datetime(data['Date'])
 data['Date'] = data['Date'].dt.date # Removes the Time Portion
-# print(data.head())
-# print(data.tail())
+data['Day'] = data['Date'].dt.day_of_week
+data['Day'] = data['Day'].map({
+    0: 'Monday',
+    1: 'Tuesday',
+    2: 'Wednesday',
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday'
+})
+print(data.head())
+print(data.tail())
 
 # Part 2
 # Line Plot
-# data.plot(x='Date', y='Instagram reach', title='Instagram Reach over Time')
+data.plot(x='Date', y='Instagram reach', title='Instagram Reach over Time')
 
 # Bar Plot for 30 Days
 d30 = data[:30]
-# d30.plot.bar(x='Date', title='Instagram Reach over Time - 30 Days')
+d30.plot.bar(x='Date', title='Instagram Reach over Time - 30 Days')
 
 # Box Plot
 data.plot.box(title='Instagram Reach over Time')
 
-plt.show()
+# plt.show()
